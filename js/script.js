2617526e23a2d6dd8c9f324f089dba2fa9f0f792
LINDECKER Charles <92403010+LINDECKER-Charles@users.noreply.github.com>
2026-02-12 14:42:20 +0100

    feat(burger) : add menu burger

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const articles = document.querySelectorAll('article');
-console.log(articles);
+/* Menu Burger */
+const burgerMenuOpen = document.querySelector("#burger-open");
+const burgerMenuClose = document.querySelector("#burger-close");
+const nav = document.querySelector("#nav-ul");
 
-articles.forEach(article => {
-    const figure = article.querySelector('figure');
-    const p = article.querySelector('p');
-    const imgs = figure.querySelectorAll('img');
-    /* console.log(figure, p, imgs); */
-    imgs.forEach(img => {
-        img.addEventListener('click', () => {
-            imgs[0].classList.toggle('hidden');
-            imgs[1].classList.toggle('hidden');
-            p.classList.toggle('accordion-body-open');
-        });
-    });
-    
+console.log(burgerMenuClose, burgerMenuOpen);
+
+burgerMenuOpen.addEventListener("click", () => {
+    nav.classList.remove("-translate-x-full");
+    burgerMenuOpen.classList.add("opacity-0", "-rotate-90", "pointer-events-none");
+    burgerMenuClose.classList.remove("opacity-0", "rotate-90", "pointer-events-none");
 });
+
+burgerMenuClose.addEventListener("click", () => {
+    nav.classList.add("-translate-x-full");
+    burgerMenuOpen.classList.remove("opacity-0", "-rotate-90", "pointer-events-none");
+    burgerMenuClose.classList.add("opacity-0", "rotate-90", "pointer-events-none");
+});
+
