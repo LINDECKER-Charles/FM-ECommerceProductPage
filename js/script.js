8c651922fcd7a153987f080cdf417c095aeeef26
LINDECKER Charles <92403010+LINDECKER-Charles@users.noreply.github.com>
2026-02-12 15:03:03 +0100

    feat(caroussel) : add image selector

--- js/script.js
+++ js/script.js
@@ -17,3 +17,25 @@ burgerMenuClose.addEventListener("click", () => {
     burgerMenuClose.classList.add("opacity-0", "rotate-90", "pointer-events-none");
 });
 
+/* Slider */
+
+const buttons = document.querySelectorAll("button");
+const mainImg = document.querySelector("#current-image");
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.setAttribute("aria-current", "true");
+    button.children[0].classList.add("selected-img");
+    button.children[1].classList.remove("hidden");
+
+    mainImg.setAttribute("src", button.children[0].getAttribute("src").replace("-thumbnail", ""))
+    buttons.forEach((b) => {
+        if(b !== button){
+            b.setAttribute("aria-current", "false");
+            b.children[0].classList.remove("selected-img");
+            b.children[1].classList.add("hidden");
+        }
+    });
+  });
+});
